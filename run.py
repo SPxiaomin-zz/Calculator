6231649402c099c1046fbc903d3a9762fe9371a2
gujunmin <3013366498@qq.com>
2015-05-26 15:33:19 +0800

    kerney finish

--- run.py
+++ run.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template, request, jsonfy
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/cal')
 def c():
-    return render_template(ca.html)
+    return render_template('ca1.html')
 
 @app.route('/calcal')
 def cc():
-    a = request.args.get('a', 0, type='float')
-    b = request.args.get('b', 0, type='float')
-    return jsonfy(result=a+b)
+    a = request.args.get('a', 0, type=float)
+    b = request.args.get('b', 0, type=float)
+    c = request.args.get('operator')
+    if c == '+':
+        return jsonify(result=a+b)
+    elif c == '-':
+        return jsonify(result=a-b)
+    elif c == '*':
+        return jsonify(result=a*b)
+    elif c == '/':
+        return jsonify(result=a/b)
+    elif c == '%':
+        return jsonify(result=a%b)
 
 if __name__ == '__main__':
     app.run()
